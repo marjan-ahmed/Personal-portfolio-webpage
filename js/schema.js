d39f839a7cd467818f63bd9efbfaa1a27ccbbabf
marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-07-09 20:14:08 +0500

    first commit

---
+++ js/schema.js
@@ -0,0 +1,18 @@
+
+// schema.js
+
+const schemaMarkup = {
+    "@context": "http://schema.org",
+    "@type": "PERSONAL PORTFOLIO WEBSITE",
+    "name": "Marjan Ahmed",
+    "url": "https://www.marjanahmed.vercel.app",
+    "logo": "http://www.marjanahmed.vercel.app/images/my_pic.png",
+    "contactPoint": {
+      "@type": "PERSONAL PORTFOLIO WEBSITE",
+      "telephone": "+923062806717",
+      "contactType": "#contact"
+    }
+  };
+  
+  // Export the schema markup if needed
+  module.exports = schemaMarkup;
