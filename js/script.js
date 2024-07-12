43476bba5f52cd4f08b7312348f3b9fad187dd6f
marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-07-12 05:27:43 +0500

    extra comment clean

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Auto Typing Effect Script
+
 
 var typed = new Typed(".auto-type", {
     strings: [
@@ -10,20 +10,20 @@ var typed = new Typed(".auto-type", {
     loop: true,
   });
 
-// Loading effect
+
 
   document.addEventListener("DOMContentLoaded", function() {
   const loader = document.getElementById('loader');
   const content = document.getElementById('content');
 
-  // Show loader initially
+  
   loader.style.display = 'flex';
 
-  // Simulate content loading delay (you can remove this in production)
+  
   setTimeout(function() {
-    // Hide loader
+    
     loader.style.display = 'none';
-    // Show content
+    
     content.classList.add('loaded');
-  }, 1500); // Adjust the timeout duration as needed
+  }, 1500); 
 });
